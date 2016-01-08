8d766bdcbb8d39e440b225fad1e9ca05d05edd56
Vincent Bastos <vincent@lavalab.com.au>
2016-01-08 12:39:39 +1000

    increase version

--- isonasacs/__init__.py
+++ isonasacs/__init__.py
@@ -26,7 +26,7 @@
 # (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
 # SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 "a library to communicate with ISONAS Access Control System"
-__version__ = '0.3'
+__version__ = '0.4'
 __all__ = ['Isonasacs', 'IsonasacsError']
 
 import datetime
